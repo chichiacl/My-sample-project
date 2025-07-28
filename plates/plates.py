3723e594e436c49b38fe3ee341d0760e76a6a4bc
chichiacl <mschichiacl@gmail.com>
2025-07-28 20:45:49 +0000

    Mon, Jul 28, 2025, 10:45 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,15 +7,13 @@ def main():
 
 
 def is_valid(p):
-       if 2 <= len(s) > 6: # correct length
+       if len(s) < 2 or len(s) > 6: # correct length
         return False
 
        if not s[:2].isalpha(): # first 2 chars letter
             return False
 
-        if s.isalnum(): # no puncuation
-           return True
-        else:
+        if not s.isalnum(): # no puncuation
            return False
 
 
