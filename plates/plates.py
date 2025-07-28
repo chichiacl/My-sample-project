82e85ff7e4fc0a78fd1b2346e3068bf26ac895b6
chichiacl <mschichiacl@gmail.com>
2025-07-28 20:32:12 +0000

    Mon, Jul 28, 2025, 10:32 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,12 +10,5 @@ def is_valid(p):
        if 2 <= len(s) > 6:
         return False
 
-def two_letter(s):
-    s[0:2].isalpha()
-
-def number(n):
-    index = len(n) # 5
-    return n[index].isdigit() # true
-
 
 main()
