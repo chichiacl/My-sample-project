d1d01a358ae6d7d04b2655fc4334413fdb8b1631
chichiacl <mschichiacl@gmail.com>
2025-08-12 21:27:22 +0000

    Tue, Aug 12, 2025, 11:27 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,11 +1,11 @@
-import pyfiglet
+from pyfiglet import Figlet
 import random
 import sys
 
-if len(sys.argv) == 1:
-    # Zero if the user would like to output text in a random font.
-    
-elif len(sys.argv) == 3:
-    #Two if the user would like to output text in a specific font, in which case the first of the two should be -f or --font, and the
-else:
-    ...
+figlet = Figlet()
+
+figlet.getFonts()
+
+figlet.setFont(font=f)
+
+print(figlet.renderText(s))
