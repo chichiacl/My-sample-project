3ddf11ed6a57b787e06a67ad097915e055f90587
chichiacl <mschichiacl@gmail.com>
2025-08-12 23:21:54 +0000

    Wed, Aug 13, 2025, 1:21 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -4,7 +4,7 @@ p = inflect.engine()
 names = []
 while True:
     try:
-        names.append(input("Names: "))
+        names.append(input("Names:"))
     except EOFError:
         print(f"Adieu, adieu to {p.join(names)}")
         break
