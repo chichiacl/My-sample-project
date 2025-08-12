c0ef4613e98e42acb677289959275fbd463e98ac
chichiacl <mschichiacl@gmail.com>
2025-08-12 21:47:25 +0000

    Tue, Aug 12, 2025, 11:47 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,7 +6,9 @@ figlet = Figlet() # object
 font_List = figlet.getFonts() # list of all the fonts
 if len(sys.argv) == 1:
     #zero, choose random font
-    font = random.choice(font_List)
+    f = random.choice(font_List)
+    figlet.setFont(font=f)
+    print(figlet.renderText(s))
 elif len(sys.argv) == 3:
     # @ promt user chooses font
 else:
