9fa02a4c3fb982828b2334bb6da6d8f33d9f18ae
chichiacl <mschichiacl@gmail.com>
2025-08-12 21:25:27 +0000

    Tue, Aug 12, 2025, 11:25 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,10 +4,8 @@ import sys
 
 if len(sys.argv) == 1:
     # Zero if the user would like to output text in a random font.
-    sys.exit
-
-try:
-    len(sys.argv)
+    
+elif len(sys.argv) == 3:
     #Two if the user would like to output text in a specific font, in which case the first of the two should be -f or --font, and the
-except 
+else:
     ...
