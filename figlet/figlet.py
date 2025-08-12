ae682a92948ff23155e40f688b3aabdf1fac2ee8
chichiacl <mschichiacl@gmail.com>
2025-08-12 22:07:54 +0000

    Wed, Aug 13, 2025, 12:07 AM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -14,9 +14,13 @@ if len(sys.argv) == 1:
     print(figlet.renderText(s))
 elif len(sys.argv) == 3:
     # @ promt user chooses font
-    if
-else:
-    ...
+    if (sys.argv[1] != "-f") or (sys.argv[1] != "--font") :
+        print("Invalid usage")
+        sys.exit
+    if sys.argv[2] not in font_list:
+        print("Invalid usage")
+        sys.exit
+    
 
 
 """
