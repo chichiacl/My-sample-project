841ade7e2600daa15c380a6d8a0c1207192a5059
chichiacl <mschichiacl@gmail.com>
2025-08-13 19:19:39 +0000

    Wed, Aug 13, 2025, 9:19 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,26 +2,15 @@ import requests
 import sys
 
 if len(sys.argv) <= 1:
-    raise IndexError("Missing command-line argument")
+    sys.exit("Missing command-line argument")
+
+if not sys.argv[1].isnumeric():
+    sys.exit("Command-line argument is not a number")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
 try:
-    ...
+    response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=YourApiKey")
+    content = response.json()
+    print(content)
 except requests.RequestException:
-    ...
-"""
+    sys.exit()
