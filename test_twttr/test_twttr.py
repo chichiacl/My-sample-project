675060198b3f5dc87e4a78994b97ed8573a786d2
chichiacl <mschichiacl@gmail.com>
2025-08-16 19:40:25 +0000

    Sat, Aug 16, 2025, 9:40 PM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -3,7 +3,14 @@ def main():
 
 
 def shorten(word):
-    ...
+input = input("Input: ")
+output = ""
+for v in input:
+    if v in ["A","E","I","O","U","a","e","i","o","u"]:
+        None
+    else:
+         output = output + v
+print("Output:",output)
 
 
 if __name__ == "__main__":
