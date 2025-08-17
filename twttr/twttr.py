412711435f0fe455626d0537cab055c92c31690b
chichiacl <mschichiacl@gmail.com>
2025-08-17 11:47:41 +0000

    Sun, Aug 17, 2025, 1:47 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,16 @@
-input = input("Input: ")
-output = ""
-for v in input:
-    if v in ["A","E","I","O","U","a","e","i","o","u"]:
+def main():
+    input = input("Input: ")
+    result = shorten(input)
+    print("Output:", result)
+
+def shorten(word):
+    output = ""
+    for v in input:
+        if v in ["A","E","I","O","U","a","e","i","o","u"]:
         None
     else:
          output = output + v
-print("Output:",output)
+return output
 
+if __name__ == "__main__":
+    main()
