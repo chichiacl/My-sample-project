ccff72cc4e77557d4c22eb15e132b908f81fdb67
chichiacl <mschichiacl@gmail.com>
2025-08-17 13:47:43 +0000

    Sun, Aug 17, 2025, 3:47 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -12,11 +12,19 @@ else:
 
 def main():
     string = input("Greeting: ")
+    amount = value(string)
+    print(f"${amount}")
 
 
 def value(greeting):
-    ...
-
+    greeting = string.strip().split()
+    greeting = greeting[0]
+    if greeting.lower() == "hello" or greeting.lower() == "hello,":
+        print("$0")
+    elif greeting[0].lower() == 'h':
+        print("$20")
+    else:
+        print("$100")
 
 if __name__ == "__main__":
     main()
