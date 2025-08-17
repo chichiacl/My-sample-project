df31012c243e089665961d6a5b2f8160aba1fc63
chichiacl <mschichiacl@gmail.com>
2025-08-17 14:33:44 +0000

    Sun, Aug 17, 2025, 4:33 PM +02:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -3,3 +3,17 @@ import pytest
 
 def test_hello():
     assert value("Hello") == 0
+    assert value("Hello, world") == 0
+    assert value("Hello!") == 0
+
+def test_h():
+    assert value("How you doing?") == 20
+    assert value("How can i help you") == 0
+    assert value("Hello.") == 0
+
+
+def test_rest():
+    assert value("What's happening?") == 0
+    assert value("Welcome to the bank.") == 0
+    assert value("123! client 123!") == 0
+
