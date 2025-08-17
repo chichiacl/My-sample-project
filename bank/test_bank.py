a9093cb086687c92d6d301b70dc25d8ab56ba59a
chichiacl <mschichiacl@gmail.com>
2025-08-17 15:00:16 +0000

    Sun, Aug 17, 2025, 5:00 PM +02:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -1,19 +1,24 @@
 from bank import value
 import pytest
 
-def test_hello():
+def test_zero():
     assert value("Hello") == 0
-    assert value("Hello, world") == 0
+    assert value("HeLlo, w0rld") == 0
     assert value("Hello!") == 0
 
-def test_h():
+def test_twenty():
     assert value("How you doing?") == 20
-    assert value("How can i help you") == 20
+    assert value("H  ") == 20
     assert value("Hello.") == 20
 
-def test_rest():
+def test_hundred():
     assert value("What's happening?") == 100
     assert value("Welcome to the bank.") == 100
     assert value("123! client 123!") == 100
+
+def test_incorrect_value():
+    with pytest:raise(TypeError) :
+
+
 
 
