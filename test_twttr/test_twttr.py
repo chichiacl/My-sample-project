19559b34f2d43004122d711bd708fff3df585346
chichiacl <mschichiacl@gmail.com>
2025-08-17 12:32:58 +0000

    Sun, Aug 17, 2025, 2:32 PM +02:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -4,4 +4,4 @@ from twttr import shorten
 def test_shorten():
     assert shorten("Twitter") == "Twitter"
     assert shorten("What's your name?") == "Wht's yr nm?"
-    assert shoretn("CS50") == "CS50"
+    assert shorten("CS50") == "CS50"
