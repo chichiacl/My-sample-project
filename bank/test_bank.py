9fedf62df1b9e905cbdf8c7951f392308fa5cff5
chichiacl <mschichiacl@gmail.com>
2025-08-17 15:21:19 +0000

    Sun, Aug 17, 2025, 5:21 PM +02:00

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -1,20 +1,21 @@
 from bank import value
 import pytest
 
-def test_zero():
+def test_hello():
+    assert value("hello") == 0
     assert value("Hello") == 0
-    assert value("HeLlo, w0rld") == 0
-    assert value("Hello!") == 0
-    assert value("How you doing?") == 20
-    assert value("What's happening?") == 100
+    assert value("HELLO there") == 0
+    assert value("hello!") == 0
+    assert value("hello, world") == 0
 
+def test_h():
+    assert value("hi") == 20
+    assert value("hey there") == 20
+    assert value("howdy") == 20
+    assert value("Hmmm") == 20
 
-def test_incorrect_values():
-    ...
-
-def test_case_sensitive():
-    ...
-
-
-def test_entire_phrase():
-    ...
+def test_not_h():
+    assert value("What's up") == 100
+    assert value("Good morning") == 100
+    assert value("Bye") == 100
+    assert value("Ahoy") == 100
